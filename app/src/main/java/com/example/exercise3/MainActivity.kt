8eaf65cfb2c7523480b73d09d67f0f035b4e9259
taruc <heeci-wm17@student.tarc.edu.my>
2019-12-11 11:44:05 +0800

    init

---
+++ app/src/main/java/com/example/exercise3/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.exercise3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener {
+            textResult.text = "RM " + Calculate().toString()
+        }
+
+        buttonReset.setOnClickListener {
+            spinnerAge.setSelection(0)
+            radioGroup.clearCheck()
+
+        }
+
+    }
+
+    fun Calculate(): Double{
+        
+
+        return when(spinnerAge.selectedItemPosition){
+            0 -> 60.00
+            1 -> 70.00 +
+                    (if(radioMale.isChecked) 50.00 else 0.0)+
+                    (if(checkSmokeYes.isChecked) 100.00 else 0.0)
+            2 -> 90.00 +
+                    (if(radioMale.isChecked) 100.00 else 0.0)+
+                    (if(checkSmokeYes.isChecked) 150.00 else 0.0)
+            3 -> 120.00 +
+                    (if(radioMale.isChecked) 150.00 else 0.0)+
+                    (if(checkSmokeYes.isChecked) 200.00 else 0.0)
+            4 -> 150.00 +
+                    (if(radioMale.isChecked) 200.00 else 0.0)+
+                    (if(checkSmokeYes.isChecked) 250.00 else 0.0)
+
+            else -> 150.00 +
+                    (if(radioMale.isChecked) 200.00 else 0.0)+
+                    (if(checkSmokeYes.isChecked) 300.00 else 0.0)
+
+        }
+
+
+    }
+}
